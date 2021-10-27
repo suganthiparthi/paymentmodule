4936369a751dfee6b062e9a2f2334b7534e9abc4
suganthiparthi09 <sugansuganthi18@gmail.com>
2021-10-27 22:27:48 +0530

    firstmessage

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,31 @@
+package org.add;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GreensTech {
+public static void main(String[] args) throws IOException {
+File f = new File("C:\\Users\\Suganthi\\OneDrive\\Desktop\\New folder");
+
+boolean createNewFile = f.createNewFile();
+System.out.println(createNewFile);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
